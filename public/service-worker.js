f22fda2a21350fe54cee9f7a0185e4fe0c69cb81
Quest Lines <dev@questlines.local>
2026-09-24 17:22:20 +0000

    Use original fonts (Lilita One + Mitr)

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quest-lines-v6';
+const CACHE_NAME = 'quest-lines-v7';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (e) => {
