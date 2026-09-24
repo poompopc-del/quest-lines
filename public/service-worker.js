d1fe9080efdaf7daa23ff949cdb103951a29b5e8
Poom <poompopc@gmail.com>
2026-09-24 11:32:59 +0700

    Word Battle v3

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quest-lines-v2.1';
+const CACHE_NAME = 'quest-lines-v3';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (e) => {
