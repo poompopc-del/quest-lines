83ee2b3ac529850de70f11ee7358564a893b8317
Poom <poompopc@gmail.com>
2026-09-25 14:52:55 +0700

    v11: Endless Tower + BOOM finisher

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quest-lines-v8';
+const CACHE_NAME = 'quest-lines-v11';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (e) => {
