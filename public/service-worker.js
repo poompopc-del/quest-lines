26614aed62adbc67602f22b01d4a294d9e0c2af8
Poom <poompopc@gmail.com>
2026-09-27 00:50:10 +0700

    v14b: ELON continuous weapon combo

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quest-lines-v14';
+const CACHE_NAME = 'quest-lines-v14b';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (e) => {
