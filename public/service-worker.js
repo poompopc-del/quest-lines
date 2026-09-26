5416b1897739ab68bae953b7c7d91083a9abe59e
Poom <poompopc@gmail.com>
2026-09-27 00:04:25 +0700

    v13d: Plink, Kanit font, new Boomtos sprite + idle fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quest-lines-v13c';
+const CACHE_NAME = 'quest-lines-v13d';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (e) => {
