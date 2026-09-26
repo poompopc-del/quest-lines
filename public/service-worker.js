712a86df39d3d6df49e0eba01b810709214a796a
Poom <poompopc@gmail.com>
2026-09-26 20:28:44 +0700

    v12b: smoother samurai animations

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quest-lines-v12';
+const CACHE_NAME = 'quest-lines-v12b';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (e) => {
